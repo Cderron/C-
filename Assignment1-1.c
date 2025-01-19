e65a968925bcab27f042fbc1d8e415c1fd6dd0a7
Cderron <derroncoghiel860@gmail.com>
2025-01-19 15:45:04 -0500

    Add files via upload

---
+++ Assignment1-1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char *info1 = "Lucinda Potter\n6/24/1992\nwork 000-000-0101\ncell 000-000-0189";
+    printf("%s\n\n", info1);
+    char *info2 = "John Smith\n8/15/1978\nwork 000-111-0101\ncell 000-111-0189";
+    printf("%s\n\n", info2);
+    char *info3 = "Chris Johnson\n12/1/1987\nwork 000-222-0101\ncell 000-222-0189";
+    printf("%s\n\n", info3);
+    char* info4 = "Robert Hall\n2/27/1949\nwork 000-444-0101\ncell 000-444-0189";
+    printf("%s", info4);
+}
+
