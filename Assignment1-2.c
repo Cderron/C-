cb38190c184c2fbf8b505f325b16ff27033665eb
Cderron <derroncoghiel860@gmail.com>
2025-01-19 15:46:53 -0500

    Add files via upload

---
+++ Assignment1-2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char *song1 = "Oh, well imagine /\nAs I'm pacing the pews in a church corridor/\nAnd I can't help but to hear/\nNo, I can't help but to hear an exchanging of words...";
+    printf("%s\n", song1);
+    char *song2 = "Oh-oh-oh-oh-oh,\noh-oh-oh-oh,\noh-oh-oh/\nCaught in a bad romance...";
+    printf("%s\n", song2);
+    char *song3 = "Coming out of my cage\nAnd I've been doing just fine\nGotta gotta be down\nBecause I want it all";
+    printf("%s\n", song3);
+    char *song4 = "Now, this is a story all about how\nMy life got flipped-turned upside down\nAnd I'd like to take a minute\nJust sit right there\nI'll tell you how I became the prince of a town called Bel-Air";
+    printf("%s", song4);
+}
+
