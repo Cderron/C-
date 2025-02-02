d09f44edc33100b73e0eacfb3afe982de26c5752
Cderron <derroncoghiel860@gmail.com>
2025-02-01 19:04:36 -0500

    Add files via upload

---
+++ Perfect.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+    int perfectNumber = 0;
+    printf("Please input a perfect number: ");
+    scanf("%d", &perfectNumber);
+    
+    //happy path
+    if (perfectNumber == 6 || perfectNumber == 28 || perfectNumber == 496 || perfectNumber == 8128 ){
+        printf("Perfect Number");
+    }
+     //not happy path
+    else if (perfectNumber < 6 ) {
+        printf("Not A Perfect Number");
+    }
+    //not happy path
+    else if (perfectNumber > 6 ) {
+        printf("Not A Perfect Number");
+    }
+    //not happy path
+    else if (perfectNumber < 28) {
+        printf("Not A Perfect Number");
+    }
+    //not happy path
+    else if (perfectNumber < 28 ) {
+        printf("Not A Perfect Number");
+    }
+    //not happy path
+    else if (perfectNumber < 496) {
+        printf("Not A Perfect Number");
+    }
+    //not happy path
+    else if (perfectNumber > 496) {
+        printf("Not A Perfect Number");
+    }
+    //not happy path
+    else if (perfectNumber < 8128) {
+        printf("Not A Perfect Number");
+    }
+    //not happy path
+    else if (perfectNumber > 8128) {
+        printf("Not A Perfect Number");
+    }
+    return 0;
+
+}
